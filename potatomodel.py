93d62df571a7ed8c6afd6cf91fcc46bf7bbf4fc5
bishal9874 <bm8130377@gmail.com>
2022-11-23 18:54:01 +0530

    updated module

--- potatomodel.py
+++ potatomodel.py
@@ -6,7 +6,7 @@ BATCH_SIZE = 32
 CHANNELS = 3
 EPOCHS = 50
 dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    "potato disease/disease",
+    "disease",
     shuffle=True,
     image_size=(IMAGE_SIZE,IMAGE_SIZE),
     batch_size= BATCH_SIZE
@@ -23,20 +23,132 @@ for image_batch,label_batch in dataset.take(1):
         plt.axis("off")
         plt.title(class_name[label_batch[i]])
         # plt.show()
-train_size=0.8
-dataset.take(10)
-print(len(dataset)*train_size)
-train_ds = dataset.take(54)
+# train_size=0.8
+# dataset.take(10)
+# print(len(dataset)*train_size)
+# train_ds = dataset.take(54)
+# print(len(train_ds))
+# test_ds = dataset.skip(54)
+# print(len(test_ds))
+# val_size= 0.1
+# print(len(dataset)*val_size)
+# val_ds = test_ds.take(6)
+# print(len(val_ds))
+# test_ds = test_ds.skip(6)
+# print(len(test_ds))
+def get_daatset_partitions_tf(ds,train_split=0.8,val_split = 0.1,
+test_split=0.1,shuffle=True, shuffle_size=10000):
+    ds_size = len(ds)
+    if shuffle:
+        ds = ds.shuffle(shuffle_size,seed=12)
+    train_size = int(train_split *ds_size)
+    val_size = int(val_split * ds_size)
+    train_ds = ds.take(train_size)
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+
+    return train_ds,val_ds,test_ds
+
+
+train_ds,val_ds,test_ds = get_daatset_partitions_tf(dataset)
+# print(image_batch[0])
+# print(label_batch.numpy())
+
 print(len(train_ds))
-test_ds = dataset.skip(54)
-print(len(test_ds))
-val_size= 0.1
-print(len(dataset)*val_size)
-val_ds = test_ds.take(6)
 print(len(val_ds))
-test_ds = test_ds.skip(6)
 print(len(test_ds))
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+#Creating a Layer for Resizing and Normalization
+
+resize_and_rescaling = tf.keras.Sequential([
+    tf.keras.layers.experimental.preprocessing.Resizing(IMAGE_SIZE,IMAGE_SIZE),
+    tf.keras.layers.experimental.preprocessing.Rescaling(1.0/255)
+])
+#Data Augmentation
+#Data Augmentation is needed when we have less data,
+#this boosts the accuracy of our model by augmenting the data.
+
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+  tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+])
+
+                                                # Model Architecture
+# We use a CNN coupled with a Softmax activation in the output layer. 
+# We also add the initial layers for resizing, normalization and Data Augmentation.
+# We are going to use convolutional neural network (CNN) here.
+# CNN is popular for image classification tasks. Watch below video to understand fundamentals of CNN
+
+input_shape = (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
+n_classes = 3
+
+model = models.Sequential([
+    resize_and_rescaling,
+    layers.Conv2D(32, kernel_size = (3,3), activation='relu', input_shape=input_shape),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64,  kernel_size = (3,3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64,  kernel_size = (3,3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(n_classes, activation='softmax'),
+])
+
+model.build(input_shape=input_shape)
+model.summary()
 
 
-    # print(image_batch[0])
-    # print(label_batch.numpy())
+# Compiling the Model
+# We use adam Optimizer, SparseCategoricalCrossentropy for losses, accuracy as a metric
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_ds,
+    batch_size=BATCH_SIZE,
+    validation_data=val_ds,
+    verbose=1,
+    epochs=EPOCHS,
+)
+score = model.evaluate(test_ds)
+score
+history
+history.params
+history.history.keys()
+type(history.history['loss'])
+len(history.history['loss'])
+history.history['loss'][:5] # show loss for first 5 epochs
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(range(EPOCHS), acc, label='Training Accuracy')
+plt.plot(range(EPOCHS), val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(range(EPOCHS), loss, label='Training Loss')
+plt.plot(range(EPOCHS), val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
